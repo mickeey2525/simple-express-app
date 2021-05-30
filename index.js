457788d5e192513813a3cc155b931b94a30a154d
mickeey2525 <m.tachibana25@gmail.com>
2021-05-30 20:08:52 +0900

    commit first

--- index.js
+++ index.js
@@ -4,5 +4,9 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
+    res.send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
